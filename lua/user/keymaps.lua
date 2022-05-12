f0900304e8a516112757ef75869fa261c99f71bd
leosun <leosunsl@outlook.com>
2022-05-12 11:43:45 +0800

    remove keymappings for windows switching

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -19,12 +19,6 @@ vim.g.maplocalleader = " "
 --   command_mode = "c",
 
 -- Normal --
--- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
-
 keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 
 -- Resize with arrows
