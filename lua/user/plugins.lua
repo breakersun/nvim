e172ed6fa2cc73d4ab9b755b0d04549cd468ecfb
leosun <leosunsl@outlook.com>
2022-05-12 16:38:24 +0800

    simplify packer settings follow official instructions & install nlsp-settings.nvim

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -3,14 +3,7 @@ local fn = vim.fn
 -- Automatically install packer
 local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
+  packer_bootstrap = fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
   print "Installing packer close and reopen Neovim..."
   vim.cmd [[packadd packer.nvim]]
 end
@@ -53,6 +46,7 @@ return packer.startup({function(use)
   -- LSP
   use 'neovim/nvim-lspconfig'
   use 'williamboman/nvim-lsp-installer'
+  use "tamago324/nlsp-settings.nvim" -- language server settings defined in json format
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
