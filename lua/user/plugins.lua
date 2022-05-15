9929fca4794395ed1b2ede5028b34ab8af1a16c9
leosun <leosunsl@outlook.com>
2022-05-16 07:52:36 +0800

    add telescope, it just works

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -48,6 +48,8 @@ return packer.startup({function(use)
   use 'neovim/nvim-lspconfig'
   use 'williamboman/nvim-lsp-installer'
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json format
+  -- Telescope
+  use "nvim-telescope/telescope.nvim"
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
