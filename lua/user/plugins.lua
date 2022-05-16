6c72f0f3672729fd6e804d018dc41e66b21dd2fb
leosun <leosunsl@outlook.com>
2022-05-16 14:09:50 +0800

    add gitsigns

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -56,6 +56,8 @@ return packer.startup({function(use)
   use "windwp/nvim-autopairs"
   -- comment
   use "numToStr/Comment.nvim"
+  -- git
+  use "lewis6991/gitsigns.nvim"
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
