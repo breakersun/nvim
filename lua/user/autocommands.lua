10369b79db07762047b6e479d3049c98f962e17b
leosun <leosunsl@outlook.com>
2022-05-19 14:54:31 +0800

    treat .h as c file instead of c++

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -4,5 +4,6 @@ vim.cmd [[
   autocmd FileType c setlocal shiftwidth=4 softtabstop=4 expandtab
   autocmd FileType cpp setlocal shiftwidth=4 softtabstop=4 expandtab
   autocmd FileType make set noexpandtab
+  autocmd BufNewFile,BufRead *.h setlocal filetype=c
   augroup end
 ]]
