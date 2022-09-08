111bac8a873e0f8b08dd456bbf945276128c3a26
leosun <leosunsl@outlook.com>
2022-09-08 17:00:12 +0800

    remove deprecated options of nvim-tree

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -29,10 +29,6 @@ nvim_tree.setup {
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = true,
-  update_to_buf_dir = {
-    enable = true,
-    auto_open = true,
-  },
   diagnostics = {
     enable = true,
     icons = {
@@ -57,7 +53,6 @@ nvim_tree.setup {
     height = 30,
     hide_root_folder = false,
     side = "right",
-    auto_resize = true,
     mappings = {
       custom_only = false,
       list = {
