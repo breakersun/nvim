fc39973bcab184385fca8166d823ebc5bcce9b54
leosun <leosunsl@outlook.com>
2022-09-15 18:19:18 +0800

    remove tab options leave it to plugin to take care; enable icons in wilder

--- lua/user/wilder.lua
+++ lua/user/wilder.lua
@@ -15,6 +15,8 @@ wilder.set_option('renderer', wilder.popupmenu_renderer(
     min_width = '100%', -- minimum height of the popupmenu, can also be a number
     min_height = '50%', -- to set a fixed height, set max_height to the same value
     reverse = 0,        -- if 1, shows the candidates from bottom to top
+    left = {' ', wilder.popupmenu_devicons()},
+    right = {' ', wilder.popupmenu_scrollbar()},
   })
 ))
 
