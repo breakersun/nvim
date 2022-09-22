63f1c3c458e6b94378672be4075a0df6db47b031
leosun <leosunsl@outlook.com>
2022-09-22 15:45:47 +0800

    refactor hop.nvim setup methods to its own module

--- lua/user/hop_nvim.lua
+++ lua/user/hop_nvim.lua
@@ -1,3 +1,11 @@
+local status_ok, hopnvim = pcall(require, 'hop')
+
+if not status_ok then
+    return
+end
+
+hopnvim.setup { keys = 'etovxqpdygfblzhckisuran' }
+
 -- place this in one of your configuration file(s)
 vim.api.nvim_set_keymap('', 'f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>", {})
 vim.api.nvim_set_keymap('', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", {})
