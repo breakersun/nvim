fd88db4f2f4b01011d5e25c06f5235ad598d865d
leosun <leosunsl@outlook.com>
2022-09-22 15:39:50 +0800

    hop.nvim just works

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -89,6 +89,14 @@ return packer.startup({ function(use)
         -- Uncomment next line if you want to follow only stable versions
         -- tag = "*"
     }
+    use {
+        'phaazon/hop.nvim',
+        branch = 'v2', -- optional but strongly recommended
+        config = function()
+            -- you can configure Hop the way you like here; see :h hop-config
+            require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+        end
+    }
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if packer_bootstrap then
