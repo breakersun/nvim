5fcefc848ffd5d8f0e24cba59502ea49b386f336
leosun <leosunsl@outlook.com>
2022-09-26 08:49:50 +0800

    refactor part of keymappings in whichkey to a more lua way

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -108,30 +108,24 @@ local mappings = {
 
   g = {
     name = "Git",
-    g = { "<cmd>lua _LAZYGIT_TOGGLE()<CR>", "Lazygit" },
-    j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
-    k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
-    l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
-    p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
-    r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
-    R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
-    s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
-    u = {
-      "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
-      "Undo Stage Hunk",
-    },
+    g = { _LAZYGIT_TOGGLE, "Lazygit" },
+    j = { require 'gitsigns'.next_hunk, "Next Hunk" },
+    k = { require 'gitsigns'.prev_hunk, "Prev Hunk" },
+    l = { require 'gitsigns'.blame_line, "Blame" },
+    p = { require 'gitsigns'.preview_hunk, "Preview Hunk" },
+    r = { require 'gitsigns'.reset_hunk, "Reset Hunk" },
+    R = { require 'gitsigns'.reset_buffer, "Reset Buffer" },
+    s = { require 'gitsigns'.stage_hunk, "Stage Hunk" },
+    u = { require 'gitsigns'.undo_stage_hunk, "Undo Stage Hunk", },
     o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
     b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
     c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
-    d = {
-      "<cmd>Gitsigns diffthis HEAD<cr>",
-      "Diff",
-    },
+    d = { "<cmd>Gitsigns diffthis HEAD<cr>", "Diff", },
   },
 
   l = {
     name = "LSP",
-    a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
+    a = { vim.lsp.buf.code_action, "Code Action" },
     d = {
       "<cmd>Telescope lsp_document_diagnostics<cr>",
       "Document Diagnostics",
@@ -152,7 +146,7 @@ local mappings = {
       "Prev Diagnostic",
     },
     l = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
-    q = { "<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>", "Quickfix" },
+    q = { vim.lsp.diagnostic.set_loclist, "Quickfix" },
     r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
     s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
     S = {
