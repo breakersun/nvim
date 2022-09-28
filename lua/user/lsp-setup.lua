792e89c56ab4664bdd1c152b66538d2ed3780aca
leosun <leosunsl@outlook.com>
2022-09-28 09:48:42 +0800

    fix usage error

--- lua/user/lsp-setup.lua
+++ lua/user/lsp-setup.lua
@@ -1,56 +1,18 @@
-require('lsp-setup').setup({
-    -- Default mappings
-    -- gD = 'lua vim.lsp.buf.declaration()',
-    -- gd = 'lua vim.lsp.buf.definition()',
-    -- gt = 'lua vim.lsp.buf.type_definition()',
-    -- gi = 'lua vim.lsp.buf.implementation()',
-    -- gr = 'lua vim.lsp.buf.references()',
-    -- K = 'lua vim.lsp.buf.hover()',
-    -- ['<C-k>'] = 'lua vim.lsp.buf.signature_help()',
-    -- ['<space>rn'] = 'lua vim.lsp.buf.rename()',
-    -- ['<space>ca'] = 'lua vim.lsp.buf.code_action()',
-    -- ['<space>f'] = 'lua vim.lsp.buf.formatting()',
-    -- ['<space>e'] = 'lua vim.diagnostic.open_float()',
-    -- ['[d'] = 'lua vim.diagnostic.goto_prev()',
-    -- [']d'] = 'lua vim.diagnostic.goto_next()',
-    default_mappings = true,
-    -- Custom mappings, will overwrite the default mappings for the same key
-    -- Example mappings for telescope pickers:
-    -- gd = 'lua require"telescope.builtin".lsp_definitions()',
-    -- gi = 'lua require"telescope.builtin".lsp_implementations()',
-    -- gr = 'lua require"telescope.builtin".lsp_references()',
-    mappings = {},
-    -- Global on_attach
-    on_attach = function(client, bufnr)
-        -- Support custom the on_attach function for global
-        -- Formatting on save as default
-        require('lsp-setup.utils').format_on_save(client)
-    end,
-    -- Global capabilities
-    capabilities = vim.lsp.protocol.make_client_capabilities(),
-    -- Configuration of LSP servers
-    servers = { }
-})
+local _, setuptool = pcall(require, 'lsp-setup')
+if not _ then return end
 
-require'lspconfig'.sumneko_lua.setup {
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
-}
+setuptool.setup({
+    servers = {
+        sumneko_lua = {
+            settings = {
+                Lua = {
+                    runtime = { version = 'LuaJIT', },
+                    diagnostics = { globals = { 'vim' }, },
+                    -- time-consuming : this could index nvim-lua lib everytime
+                    -- workspace = { library = vim.api.nvim_get_runtime_file("", true), },
+                    telemetry = { enable = false, },
+                },
+            },
+        },
+    }
+})
