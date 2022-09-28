6b3eee66170d0a2d77294556fafaf183add41bac
leosun <leosunsl@outlook.com>
2022-09-28 12:02:12 +0800

    sync keymaps for lsp

--- lua/user/lsp-setup.lua
+++ lua/user/lsp-setup.lua
@@ -2,6 +2,23 @@ local _, setuptool = pcall(require, 'lsp-setup')
 if not _ then return end
 
 setuptool.setup({
+    default_mappings = false,
+    mappings = {
+        gD = 'lua vim.lsp.buf.declaration()',
+        gd = 'lua vim.lsp.buf.definition()',
+        --[[ gt = 'lua vim.lsp.buf.type_definition()', ]]
+        gi = 'lua vim.lsp.buf.implementation()',
+        gr = 'lua vim.lsp.buf.references()',
+        K = 'lua vim.lsp.buf.hover()',
+        ['<C-k>'] = 'lua vim.lsp.buf.signature_help()',
+        ['<leader>rn'] = 'lua vim.lsp.buf.rename()',
+        ['<leader>ca'] = 'lua vim.lsp.buf.code_action()',
+        --[[ ['<space>f'] = 'lua vim.lsp.buf.formatting()', ]]
+        ['gl'] = 'lua vim.diagnostic.open_float()',
+        ['[d'] = 'lua vim.diagnostic.goto_prev()',
+        [']d'] = 'lua vim.diagnostic.goto_next()',
+        ['<leader>q'] = 'lua vim.diagnostic.setloclist()'
+    },
     servers = {
         sumneko_lua = {
             settings = {
@@ -14,6 +31,6 @@ setuptool.setup({
                 },
             },
         },
-        clangd = { },
+        clangd = {},
     }
 })
