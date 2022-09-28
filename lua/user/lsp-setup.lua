8d45d60b8001ebbbba21d1cda5c9cd624cc6b46a
leosun <leosunsl@outlook.com>
2022-09-28 13:51:03 +0800

    porting a highlight doc func from legacy, lets see what could it be

--- lua/user/lsp-setup.lua
+++ lua/user/lsp-setup.lua
@@ -4,6 +4,22 @@ if not _ then return end
 -- UI settings for lsp
 require('user.lsp-ui').setup()
 
+local function lsp_highlight_document(client)
+    -- Set autocommands conditional on server_capabilities
+    if client.resolved_capabilities.document_highlight then
+        vim.api.nvim_exec(
+            [[
+      augroup lsp_document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+    ]]       ,
+            false
+        )
+    end
+end
+
 setuptool.setup({
     default_mappings = false,
     mappings = {
@@ -22,6 +38,7 @@ setuptool.setup({
         [']d'] = 'lua vim.diagnostic.goto_next()',
         ['<leader>q'] = 'lua vim.diagnostic.setloclist()'
     },
+    on_attach = lsp_highlight_document;
     servers = {
         sumneko_lua = {
             settings = {
