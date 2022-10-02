0d3bc23317d358798ef4bb096127736abc3e68a1
breakersun <leosunsl@outlook.com>
2022-10-02 18:31:13 +0800

    bring up 2 brandnew options on v0.8.0, lets see how it is going

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,7 +1,7 @@
 local options = {
     backup = false, -- creates a backup file
     clipboard = "unnamedplus", -- allows neovim to access the system clipboard
-    cmdheight = 2, -- more space in the neovim command line for displaying messages
+    cmdheight = 0, -- more space in the neovim command line for displaying messages
     completeopt = { "menuone", "noselect" }, -- mostly just for cmp
     conceallevel = 0, -- so that `` is visible in markdown files
     fileencoding = "utf-8", -- the encoding written to a file
@@ -37,6 +37,7 @@ local options = {
     listchars = "tab:▸\\ ,eol:¬,trail:·",
     showmatch = true,
     showcmd = true,
+    winbar = "%m %f"
 }
 
 vim.opt.shortmess:append "c"
