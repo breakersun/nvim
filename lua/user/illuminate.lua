1e1c511409fd2b4bb13c16e0d05c9ec7f3fcfef6
leosun <leosunsl@outlook.com>
2022-10-09 08:59:41 +0800

    disable illuminate in nvim-tree

---
+++ lua/user/illuminate.lua
@@ -0,0 +1,10 @@
+local _, illuminate = pcall(require, 'illuminate')
+if not _ then return end
+
+illuminate.configure({
+    filetypes_denylist = {
+        'dirvish',
+        'fugitive',
+        'NvimTree'
+    },
+})
