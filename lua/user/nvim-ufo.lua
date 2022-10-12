28f05a724ba71d8e160907859272a5f0c4241705
leosun <leosunsl@outlook.com>
2022-10-12 14:59:28 +0800

    nvim-ufo: using tree-sitter as source instead of lsp

--- lua/user/nvim-ufo.lua
+++ lua/user/nvim-ufo.lua
@@ -1,9 +1,11 @@
-local status_ok, ufo
-status_ok, ufo = pcall(require, 'ufo')
-if not status_ok then
-    return
-end
-require('ufo').setup()
+local status_ok, ufo = pcall(require, 'ufo')
+if not status_ok then return end
+
+ufo.setup({
+    provider_selector = function(bufnr, filetype, buftype)
+        return { 'treesitter', 'indent' }
+    end
+})
 
 vim.o.foldcolumn = '1'
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
