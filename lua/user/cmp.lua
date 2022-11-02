206c9b62329027e88b233e9435c5a8dfe35c28ed
leosun <leosunsl@outlook.com>
2022-11-02 08:42:05 +0800

    enable some snipets for markdown & plantuml in markdown

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -9,6 +9,7 @@ if not snip_status_ok then
 end
 
 require("luasnip/loaders/from_vscode").lazy_load()
+luasnip.filetype_extend("markdown", {"markdown", "plantuml"})
 
 local check_backspace = function()
     local col = vim.fn.col "." - 1
