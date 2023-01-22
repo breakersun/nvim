994551226188421f6c2b490c5125184867d9240d
breakersun <leosunsl@outlook.com>
2023-01-22 19:31:19 +0800

    lazy cmp

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,16 +8,6 @@ return {
     end,
   },
   'machakann/vim-highlightedyank',
-  -- cmp
-  'hrsh7th/cmp-buffer',
-  'hrsh7th/cmp-path',
-  'hrsh7th/cmp-cmdline',
-  'hrsh7th/nvim-cmp',
-  'saadparwaiz1/cmp_luasnip',
-  'hrsh7th/cmp-nvim-lsp',
-  'hrsh7th/cmp-nvim-lua',
-  'L3MON4D3/LuaSnip',
-  'rafamadriz/friendly-snippets',
   -- LSP
   'neovim/nvim-lspconfig',
   {
