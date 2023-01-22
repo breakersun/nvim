06b311217c4a565b6dcb5a04ed450f604c817fd4
breakersun <leosunsl@outlook.com>
2023-01-22 09:14:31 +0800

    need hop to start at buffer reading, not just key stroke

--- lua/plugins/hop_nvim.lua
+++ lua/plugins/hop_nvim.lua
@@ -8,17 +8,19 @@ return {
     end
 
     hopnvim.setup { keys = 'etovxqpdygfblzhckisuran' }
+    vim.api.nvim_set_keymap('', 'f',
+      "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>"
+      , {})
+    vim.api.nvim_set_keymap('', 'F',
+      "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>"
+      , {})
+    vim.api.nvim_set_keymap('', 't',
+      "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>"
+      , {})
+    vim.api.nvim_set_keymap('', 'T',
+      "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<cr>"
+      , {})
+    vim.api.nvim_set_keymap("n", "S", "<cmd>HopChar2<CR>", { noremap = false })
   end,
-  keys = {
-    { "f",
-      "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>",
-      desc = 'forword with hop' },
-    { "F",
-      "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>" },
-    { "t",
-      "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>" },
-    { "T",
-      "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<cr>" },
-    { "S", "<cmd>HopChar2<CR>" }
-  }
+  event = "BufReadPost",
 }
