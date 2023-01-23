8d24558815a532d69e2f3cb69f54556183f04bc9
leosun <leosunsl@outlook.com>
2023-01-23 17:02:06 +0800

    lazy tokyonight

--- init.lua
+++ init.lua
@@ -1,7 +1,6 @@
 require "user.options"
 require "user.keymaps"
 require "user.plugins"
-require "user.colorschemes"
 require "user.cmp"
 require "user.telescope"
 require "user.treesitter"
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,12 @@
 return {
-  'folke/tokyonight.nvim',
+  {
+    "folke/tokyonight.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
   'machakann/vim-highlightedyank',
   -- cmp
   'hrsh7th/cmp-buffer',
@@ -22,34 +29,34 @@ return {
     }
   },
   -- Telescope
-  {"nvim-telescope/telescope.nvim", dependencies = "nvim-lua/plenary.nvim"},
+  { "nvim-telescope/telescope.nvim", dependencies = "nvim-lua/plenary.nvim" },
   -- Treesitter
   { "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     dependencies =
-      {'nvim-treesitter/nvim-treesitter-textobjects'},
+    { 'nvim-treesitter/nvim-treesitter-textobjects' },
     event = "BufReadPost",
   },
-  {"JoosepAlviste/nvim-ts-context-commentstring", event = "BufReadPost",},
-  {"p00f/nvim-ts-rainbow", event = "BufReadPost",},
+  { "JoosepAlviste/nvim-ts-context-commentstring", event = "BufReadPost", },
+  { "p00f/nvim-ts-rainbow", event = "BufReadPost", },
   "moll/vim-bbye", -- Bdelete, close buffer but not quit nvim
   -- toggleterm
   "akinsho/toggleterm.nvim",
   -- nvim-tree
   { 'kyazdani42/nvim-tree.lua', dependencies = 'kyazdani42/nvim-web-devicons', tag = 'nightly' },
-  {'tpope/vim-sleuth', event = "BufReadPost"},
+  { 'tpope/vim-sleuth', event = "BufReadPost" },
   { "iamcco/markdown-preview.nvim",
     build = "cd app && npm install",
     setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
     ft = { "markdown" },
   },
-  {'ferrine/md-img-paste.vim', ft = {"markdown"}},
+  { 'ferrine/md-img-paste.vim', ft = { "markdown" } },
   'tpope/vim-dispatch',
   { 'nvim-telescope/telescope-ui-select.nvim' },
   { 'nvim-telescope/telescope-project.nvim' },
-  {'ivechan/gtags.vim', event = "BufReadPost"},
-  {'junegunn/vim-easy-align', event = "BufReadPost",},
-  {'tpope/vim-unimpaired', event = "BufReadPost",},
+  { 'ivechan/gtags.vim', event = "BufReadPost" },
+  { 'junegunn/vim-easy-align', event = "BufReadPost", },
+  { 'tpope/vim-unimpaired', event = "BufReadPost", },
   'tpope/vim-fugitive',
-  {'p00f/clangd_extensions.nvim', event = "BufReadPost",},
+  { 'p00f/clangd_extensions.nvim', event = "BufReadPost", },
 }
