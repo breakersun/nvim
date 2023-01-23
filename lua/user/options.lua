c633c23604454b414f0da2928c30c448971eece7
leosun <leosunsl@outlook.com>
2023-01-23 17:21:59 +0800

    cmd height as 1 is the default value, so remove it

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,7 +1,6 @@
 local options = {
   backup = false, -- creates a backup file
   clipboard = "unnamedplus", -- allows neovim to access the system clipboard
-  cmdheight = 1, -- more space in the neovim command line for displaying messages
   completeopt = { "menuone", "noselect" }, -- mostly just for cmp
   fileencoding = "utf-8", -- the encoding written to a file
   ignorecase = true, -- ignore case in search patterns
