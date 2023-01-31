64d2d2cf84bb74d0ef051e68f6a7a12f28e3874f
leosun <leosunsl@outlook.com>
2023-02-01 07:35:22 +0800

    remove unused setup in lsp-setup

--- lua/user/lsp-setup.lua
+++ lua/user/lsp-setup.lua
@@ -4,22 +4,6 @@ if not _ then return end
 -- UI settings for lsp
 require('user.lsp-ui').setup()
 
-local function lsp_highlight_document(client, bufnr)
-  -- Set autocommands conditional on server_capabilities
-  if client.server_capabilities.document_highlight then
-    vim.api.nvim_exec(
-      [[
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]] ,
-      false
-    )
-  end
-end
-
 setuptool.setup({
   default_mappings = false,
   mappings = {
