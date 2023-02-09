f0318f07451143316c4449bf855820118c5029fd
leosun <leosunsl@outlook.com>
2023-02-09 13:15:25 +0800

    enable cmp for searching mode

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -140,19 +140,24 @@ return {
       },
     }
 
-    -- `:` cmdline setup.
+    cmp.setup.cmdline({ '/', '?' }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = 'buffer' }
+      }
+    })
+
     cmp.setup.cmdline(':', {
       mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = 'path' }
-      }, {
+      sources = cmp.config.sources(
+        { { name = 'path' } },
         {
-          name = 'cmdline',
-          option = {
-            ignore_cmds = { 'Man', '!' }
+          { name = 'cmdline',
+            option = { ignore_cmds = { 'Man', '!' } }
           }
         }
-      })
+      )
     })
+
   end
 }
