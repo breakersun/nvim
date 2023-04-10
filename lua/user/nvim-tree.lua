1a8863031d43b82c8a86cb66b285c40859624246
leosun <leosunsl@outlook.com>
2023-04-10 10:28:58 +0800

    remove nvim-tree deprecated settings

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -20,12 +20,6 @@ local tree_cb = nvim_tree_config.nvim_tree_callback
 nvim_tree.setup {
   disable_netrw = true,
   hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = {
-    "startify",
-    "dashboard",
-    "alpha",
-  },
   open_on_tab = false,
   hijack_cursor = false,
   update_cwd = true,
@@ -65,3 +59,22 @@ nvim_tree.setup {
     relativenumber = false,
   },
 }
+
+local function open_nvim_tree(data)
+  -- buffer is a [No Name]
+  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not no_name and not directory then
+    return
+  end
+  -- change to the directory
+  if directory then
+    vim.cmd.cd(data.file)
+  end
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
