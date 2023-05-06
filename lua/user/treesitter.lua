ae75c6ee84c5d85839c23c68e4011d2b3179e379
sunlong <sunlong@tcl.com>
2023-05-06 11:27:52 +0800

    update a few plugin and fix a treesitter install issue under linux

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -3,6 +3,8 @@ if not status_ok then
   return
 end
 
+require("nvim-treesitter.install").prefer_git = true
+
 configs.setup {
   ensure_installed = { "c", "python", "lua" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
