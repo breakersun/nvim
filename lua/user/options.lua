2dd1b38258379a3eea7d12ff26ee010abafe0201
leosun <leosunsl@outlook.com>
2023-05-25 17:06:00 +0800

    Revert "change default shell settings"
    This reverts commit 47b9ebc1744596841effa00c9069bd92ee4ab645.
    
    Revert "toggleterm select different shell on windows and linux"
    
    This reverts commit c7fc62a7979bc91464570b588d5fbb4ed89d2bc5.

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -39,20 +39,6 @@ local options = {
 vim.opt.shortmess:append "c"
 vim.opt.path:append "**" -- Provides tab-completion for all file-related tasks
 
-if vim.fn.has('win32') then
-  local powershell_options = {
-    shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
-    shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-    shellquote = "",
-    shellxquote = "",
-  }
-
-  for option, value in pairs(powershell_options) do
-    vim.opt[option] = value
-  end
-end
 
 for k, v in pairs(options) do
   vim.opt[k] = v
--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -15,6 +15,7 @@ toggleterm.setup({
     persist_size = true,
     direction = "float",
     close_on_exit = true,
+    shell = 'pwsh',
     float_opts = {
         border = "curved",
         winblend = 0,
