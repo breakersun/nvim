c7fc62a7979bc91464570b588d5fbb4ed89d2bc5
leosun <leosunsl@outlook.com>
2023-05-25 14:40:43 +0800

    toggleterm select different shell on windows and linux

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -3,6 +3,13 @@ if not status_ok then
     return
 end
 
+local term
+if vim.fn.has('win32') then
+  term = 'pwsh'
+else
+  term = vim.g.shell
+end
+
 toggleterm.setup({
     size = 20,
     open_mapping = [[<c-\>]],
@@ -15,7 +22,7 @@ toggleterm.setup({
     persist_size = true,
     direction = "float",
     close_on_exit = true,
-    shell = 'pwsh',
+    shell = term,
     float_opts = {
         border = "curved",
         winblend = 0,
