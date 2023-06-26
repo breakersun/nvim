5f9e13e3972d6c324ea6b4f48382aa9f91f55782
leosun <leosunsl@outlook.com>
2023-06-27 07:00:05 +0800

    refactor keys use lazy keys function

--- lua/plugins/lsp-saga.lua
+++ lua/plugins/lsp-saga.lua
@@ -1,17 +1,18 @@
 return {
     "glepnir/lspsaga.nvim",
     event = "LspAttach",
+    keys = {
+      { "gh", "<cmd>Lspsaga lsp_finder<CR>", desc = "LSP Finder (Saga)", },
+      { "K", "<cmd>Lspsaga hover_doc<CR>", desc = "LSP HoverDoc (Saga)", },
+      { "gl", "<cmd>Lspsaga show_line_diagnostics<CR>", desc = "LSP LineDiagnostics (Saga)", },
+      { "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", desc = "LSP PrevDiagnostic (Saga)", },
+      { "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", desc = "LSP NextDiagnostic (Saga)", },
+      { "<leader>lr", "<cmd>Lspsaga rename<CR>", desc = "LSP Rename (Saga)", },
+      { "<leader>lR", "<cmd>Lspsaga rename ++project<CR>", desc = "LSP RenameInProject (Saga)", },
+      { "<leader>la", "<cmd>Lspsaga code_action<CR>", desc = "LSP CodeAction (Saga)", },
+    },
     config = function()
       require("lspsaga").setup({ ui = {border = "rounded"}, symbol_in_winbar = { enable = false,} })
-      local keymap = vim.keymap.set
-      keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>")
-      keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>")
-      keymap("n", "gl", "<cmd>Lspsaga show_line_diagnostics<CR>")
-      keymap("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
-      keymap("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>")
-      keymap("n", "<leader>lr", "<cmd>Lspsaga rename<CR>")
-      keymap("n", "<leader>lR", "<cmd>Lspsaga rename ++project<CR>")
-      keymap("n", "<leader>la", "<cmd>Lspsaga code_action<CR>")
     end,
     dependencies = {
       {"kyazdani42/nvim-web-devicons"},
