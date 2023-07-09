21aa51a169808071185393a4c62197970c2b6857
leosun <leosunsl@outlook.com>
2023-07-10 07:21:22 +0800

    hop like fTtT jump with lables

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -4,7 +4,26 @@ return {
     event = "VeryLazy",
     vscode = true,
     ---@type Flash.Config
-    opts = {},
+    opts = {
+      modes = { char = {enabled = false} }
+    },
+    config = function ()
+      -- to use this, make sure to set `opts.modes.char.enabled = false`
+      require("flash").setup(opts)
+      local Config = require("flash.config")
+      local Char = require("flash.plugins.char")
+      for _, motion in ipairs({ "f", "t", "F", "T" }) do
+        vim.keymap.set({ "n", "x", "o" }, motion, function()
+          require("flash").jump(Config.get({
+            mode = "char",
+            search = {
+              mode = Char.mode(motion),
+              max_length = 1,
+            },
+          }, Char.motions[motion]))
+        end)
+      end
+    end,
     keys = {
       {
         "s",
