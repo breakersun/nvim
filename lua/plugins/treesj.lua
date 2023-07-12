c46051fb24245a82c1f9241c2a5e4099133e2fc5
leosun <leosunsl@outlook.com>
2023-07-13 07:51:20 +0800

    add TSJToggle for treesj

---
+++ lua/plugins/treesj.lua
@@ -0,0 +1,8 @@
+return {
+  'Wansmer/treesj',
+  cmd = {'TSJToggle'},
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  config = function()
+    require('treesj').setup({use_default_keymaps = false,})
+  end,
+}
