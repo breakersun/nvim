263da263dce6859c772cf1d09bca18cb1ae45246
leosun <leosunsl@outlook.com>
2023-08-23 07:49:46 +0800

    don't use hop in v mode since luasnip is in v mode
    we need tTfF for luasnip under v mode

--- lua/plugins/hop_nvim.lua
+++ lua/plugins/hop_nvim.lua
@@ -9,16 +9,16 @@ return {
     end
 
     hopnvim.setup { keys = 'etovxqpdygfblzhckisuran' }
-    vim.api.nvim_set_keymap('', 'f',
+    vim.api.nvim_set_keymap('n', 'f',
       "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>"
       , {})
-    vim.api.nvim_set_keymap('', 'F',
+    vim.api.nvim_set_keymap('n', 'F',
       "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>"
       , {})
-    vim.api.nvim_set_keymap('', 't',
+    vim.api.nvim_set_keymap('n', 't',
       "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>"
       , {})
-    vim.api.nvim_set_keymap('', 'T',
+    vim.api.nvim_set_keymap('n', 'T',
       "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<cr>"
       , {})
   end,
