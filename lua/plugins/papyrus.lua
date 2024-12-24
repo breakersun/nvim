682ac78f1daaa5b3f665d1267b42dc56c1170c5d
leosun <leosunsl@outlook.com>
2024-12-24 19:23:23 +0800

    feat: disable relative line number

--- lua/plugins/papyrus.lua
+++ lua/plugins/papyrus.lua
@@ -4,6 +4,9 @@ return {
   config = function()
     vim.g.papyrus_latex_engine = 'xelatex'
     vim.g.papyrus_viewer       = 'sumatrapdf'
-    --[[ vim.g.papyrus_pandoc_args  = '-t beamer --slide-level=2' ]]
+    vim.g.papyrus_template     = 'eisvogel.latex'
+    vim.g.papyrus_pandoc_args  = '--listings'
+    -- vim.g.papyrus_pandoc_args  = '--from markdown --template eisvogel.latex --listings'
+    -- vim.g.papyrus_pandoc_args  = '-t beamer --slide-level=2'
   end
 }
