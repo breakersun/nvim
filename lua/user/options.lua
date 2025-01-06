93882673301a8861477ec0b60823485f0c578fec
leosun <leosunsl@outlook.com>
2025-01-06 14:33:54 +0800

    remove osc52

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -53,3 +53,14 @@ vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
 
 vim.deprecate = function() end
 
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
